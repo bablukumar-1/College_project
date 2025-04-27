8426ae65b4fea91362822c97e6e85c5edc2ff846
BABLU SARKAR <156011532+bablukumar-1@users.noreply.github.com>
2025-04-27 15:21:41 +0530

    Update human.jsx
    confilict the data user

--- src/DataJson/human.jsx
+++ src/DataJson/human.jsx
@@ -1,13 +1,5 @@
 const ObjectHuman = [
-<<<<<<< HEAD
-    { name: "Er. Bablu Sarkar", role: "Student", imagePath: "/images/bablu.jpg", department: "CSE", college: "SITE", enrollment_No: "2201000000742",projectRole:"Team Leader ",batch:"2022-2026" },
-    { name: "Er. Chandra bhushan kumar", role: "Student", imagePath: "/images/bhushan.jpg", department: "CSE", college: "SITE", enrollment_No: "2201000000742",projectRole:"UI Developer ",batch:"2022-2026" },
-    { name: "Er. Gyanendra", role: "Student", imagePath: "/images/gyanendra.jpg", department: "CSE", college: "SITE", enrollment_No: "2201000000742",projectRole:"Idea & Design Developer  ",batch:"2022-2026" },
-    { name: "Er. Amarjeet Kumar", role: "Student", imagePath: "/images/amarjeet.jpg", department: "IT", college: "SITE", enrollment_No: "2201000000742",projectRole:"Team Supporter",batch:"2022-2026" },
-    { name: "Dr. Ashima Arrora", role: "Faculty", imagePath: "/images/ashima.jpg", department: "CSE", college: "SITE", enrollment_No: "2201000000742",projectRole:"Mentor " },
-]
-export default ObjectHuman
-=======
+
   {
     name: "Bablu Sarkar",
     role: "Student",
@@ -78,6 +70,6 @@ export default ObjectHuman
       batch: "2022-2026",
     },
 ];
->>>>>>> 3b5312c ( update navbar and design slider)
+
 
 export default ObjectHuman;
