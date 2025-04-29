fa344552a460cc6df72be8afe9deddac052e48fc
CBROY62 <chandrabhushankumar27080@gmail.com>
2025-04-29 10:35:55 +0530

    add some of data

--- src/DataJson/human.jsx
+++ src/DataJson/human.jsx
@@ -133,5 +133,611 @@ const ObjectHuman = [
     projectRole: "no",
     batch: "2022-2026",
   },
+ 
+
+  //  this data is rend......
+  
+    {
+      "name": "Rahul Kumar",
+      "role": "Student",
+      "imagePath": "/images/1.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000001",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Priya Sharma",
+      "role": "Student",
+      "imagePath": "/images/2.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000002",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Ankit Yadav",
+      "role": "Student",
+      "imagePath": "/images/3.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000003",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Sneha Singh",
+      "role": "Student",
+      "imagePath": "/images/4.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000004",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Vikas Gupta",
+      "role": "Student",
+      "imagePath": "/images/5.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000005",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Aditi Mehta",
+      "role": "Student",
+      "imagePath": "/images/6.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000006",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Manoj Patel",
+      "role": "Student",
+      "imagePath": "/images/7.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000007",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Pooja Tiwari",
+      "role": "Student",
+      "imagePath": "/images/8.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000008",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Ravi Verma",
+      "role": "Student",
+      "imagePath": "/images/9.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000009",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Komal Joshi",
+      "role": "Student",
+      "imagePath": "/images/10.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000010",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Ashok Reddy",
+      "role": "Student",
+      "imagePath": "/images/11.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000011",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Neha Rani",
+      "role": "Student",
+      "imagePath": "/images/12.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000012",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Karan Rawat",
+      "role": "Student",
+      "imagePath": "/images/13.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000013",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Sonal Agarwal",
+      "role": "Student",
+      "imagePath": "/images/14.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000014",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Shivani Sharma",
+      "role": "Student",
+      "imagePath": "/images/15.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000015",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Nikhil Verma",
+      "role": "Student",
+      "imagePath": "/images/16.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000016",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Sandeep Kumar",
+      "role": "Student",
+      "imagePath": "/images/17.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000017",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Tanuja Mishra",
+      "role": "Student",
+      "imagePath": "/images/18.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000018",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Jitendra Singh",
+      "role": "Student",
+      "imagePath": "/images/19.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000019",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Meenal Patel",
+      "role": "Student",
+      "imagePath": "/images/20.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000020",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Ankush Yadav",
+      "role": "Student",
+      "imagePath": "/images/21.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000021",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Simran Kaur",
+      "role": "Student",
+      "imagePath": "/images/22.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000022",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Rajeev Gupta",
+      "role": "Student",
+      "imagePath": "/images/23.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000023",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Shubham Jha",
+      "role": "Student",
+      "imagePath": "/images/24.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000024",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Ayesha Kapoor",
+      "role": "Student",
+      "imagePath": "/images/25.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000025",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Nandini Rathi",
+      "role": "Student",
+      "imagePath": "/images/26.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000026",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Praveen Kumar",
+      "role": "Student",
+      "imagePath": "/images/27.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000027",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Rohit Choudhary",
+      "role": "Student",
+      "imagePath": "/images/28.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000028",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Maya Singh",
+      "role": "Student",
+      "imagePath": "/images/29.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000029",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Vishal Agarwal",
+      "role": "Student",
+      "imagePath": "/images/30.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000030",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Arti Mishra",
+      "role": "Student",
+      "imagePath": "/images/31.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000031",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Sahil Tiwari",
+      "role": "Student",
+      "imagePath": "/images/32.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000032",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Priyanka Kaur",
+      "role": "Student",
+      "imagePath": "/images/33.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000033",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Akash Gupta",
+      "role": "Student",
+      "imagePath": "/images/34.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000034",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Laxmi Patel",
+      "role": "Student",
+      "imagePath": "/images/35.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000035",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Nitin Bansal",
+      "role": "Student",
+      "imagePath": "/images/36.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000036",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Vandana Soni",
+      "role": "Student",
+      "imagePath": "/images/37.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000037",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Mohit Sharma",
+      "role": "Student",
+      "imagePath": "/images/38.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000038",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Dinesh Kumar",
+      "role": "Student",
+      "imagePath": "/images/39.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000039",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Ruchika Mehra",
+      "role": "Student",
+      "imagePath": "/images/40.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000040",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Vineet Kumar",
+      "role": "Student",
+      "imagePath": "/images/41.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000041",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Pallavi Gupta",
+      "role": "Student",
+      "imagePath": "/images/42.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000042",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Bikram Yadav",
+      "role": "Student",
+      "imagePath": "/images/43.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000043",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Neelam Rani",
+      "role": "Student",
+      "imagePath": "/images/44.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000044",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Sanjay Kumar",
+      "role": "Student",
+      "imagePath": "/images/45.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000045",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Sakshi Dubey",
+      "role": "Student",
+      "imagePath": "/images/46.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000046",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Alok Yadav",
+      "role": "Student",
+      "imagePath": "/images/47.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000047",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Shivendra Gupta",
+      "role": "Student",
+      "imagePath": "/images/48.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000048",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Alisha Arora",
+      "role": "Student",
+      "imagePath": "/images/49.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000049",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Mukul Yadav",
+      "role": "Student",
+      "imagePath": "/images/50.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000050",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Neha Kumari",
+      "role": "Student",
+      "imagePath": "/images/51.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000051",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Mohit Rathi",
+      "role": "Student",
+      "imagePath": "/images/52.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000052",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Lalita Kumari",
+      "role": "Student",
+      "imagePath": "/images/53.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000053",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Nikita Soni",
+      "role": "Student",
+      "imagePath": "/images/54.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000054",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Kiran Verma",
+      "role": "Student",
+      "imagePath": "/images/55.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000055",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Divya Bansal",
+      "role": "Student",
+      "imagePath": "/images/56.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000056",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Gaurav Mehta",
+      "role": "Student",
+      "imagePath": "/images/57.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000057",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Ajay Kumar",
+      "role": "Student",
+      "imagePath": "/images/58.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000058",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Ravi Joshi",
+      "role": "Student",
+      "imagePath": "/images/59.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000059",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    },
+    {
+      "name": "Suman Patel",
+      "role": "Student",
+      "imagePath": "/images/60.jpg",
+      "department": "CSE",
+      "college": "SITE",
+      "enrollment_No": "2201000000060",
+      "projectRole": "UI Developer",
+      "batch": "2022-2026"
+    }
+  
+  
 ];
 export default ObjectHuman;
--- src/page/Dashboard.jsx
+++ src/page/Dashboard.jsx
@@ -51,7 +51,7 @@ const Dashboard = () => {
     <div className="main">
 
       {/* Reset Button */}
-      <div className="" style={{ marginTop: "1000px"}}>
+      <div className="" style={{ marginTop: "4500px"}}>
         <button
           className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600"
           onClick={handleReset}
